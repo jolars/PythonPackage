51ebab0c292bc1ba41f785af5fc4b9a58318b57b
Johan Larsson <johanlarsson@outlook.com>
2022-12-15 09:52:57 +0100

    chore: remove commented-out code

--- {{cookiecutter.package_name}}/tests/test.py
+++ {{cookiecutter.package_name}}/tests/test.py
@@ -3,11 +3,6 @@ import unittest
 import numpy as np
 import {{cookiecutter.package_name}}
 
-# assert pyslope.add(1, 2) == 3
-# assert pyslope.subtract(1, 2) == -1
-
-# assert pyslope.subtract(1, 2) == -1
-
 class TestLinearAlgebra(unittest.TestCase):
     def test_det(self):
         a = np.array([[1, 0.1], [0.1, 1]])
